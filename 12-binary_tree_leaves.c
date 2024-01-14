cddca79ece153c18f05044918fb4f5ca36cadc1f
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 10:04:35 -0800

    tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree-left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
