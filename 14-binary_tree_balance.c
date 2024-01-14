2a7d5b9a2a7f82ae79654092a9e51582a0cfa2ed
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 10:38:04 -0800

    tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,7 +21,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 	{
 		return (0);
 	}
