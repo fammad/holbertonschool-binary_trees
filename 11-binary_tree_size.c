1f91d00fe88de50fad63cd8c0f020ad83753d02c
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 09:56:24 -0800

    tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - Something usefull
+ * binary_tree_size - Something usefull
  * @tree: Something more usefull
  *
  * Return: Something much more usefull
