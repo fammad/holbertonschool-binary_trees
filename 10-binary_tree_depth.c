b68cd7171caac7d0146d0fa2a3881b78617aac96
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 09:51:18 -0800

    tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,12 +7,9 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 	{
 		return (0);
 	}
-	if (tree->parent != NULL)
-	{
-		return (1 + binary_tree_depth(tree->parent));
-	}
+	return (1 + binary_tree_depth(tree->parent));
 }
