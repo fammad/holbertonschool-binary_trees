6918fbf529d05d0300d3eba79cd08db0b3008138
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 10:35:28 -0800

    tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,3 +13,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
+/**
+ * binary_tree_height - Something usefull
+ * @tree: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+	{
+		return (1 + binary_tree_height(tree->left));
+	}
+	else
+	{
+		return (1 + binary_tree_height(tree->right));
+	}
+}
