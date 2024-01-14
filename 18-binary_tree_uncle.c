28d788fc1b1f49489dfd3e2b995dc18bd3a161f1
fammad <fred.mmov@gmail.com>
2024-01-14 09:48:15 -0800

    ADDED 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,10 +6,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-
-	binary_tree_t *grandparent = node->parent->parent;
+	grandparent = node->parent->parent;
 
 	if(node->parent == grandparent->left)
 		return (grandparent->right);
