16ca148726e9faf6fe3bc17ab21a6c224232f6ae
fammad <fred.mmov@gmail.com>
2024-01-14 06:34:36 -0800

    Fixed Betty Documentation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /*
- *binary_tree_node - Creates binary tree node
- *@parent: is a pointer to the parent node
- *@value: is the value to put that node
- *Return: A pointer to the newly created node
+ * binary_tree_node - Creates binary tree node
+ * @parent: is a pointer to the parent node
+ * @value: is the value to put that node
+ * Return: A pointer to the newly created node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
