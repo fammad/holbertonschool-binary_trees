822b683b03a843d38fe3803fd51b79cd9c6a5231
fammad <fred.mmov@gmail.com>
2024-01-14 06:00:00 -0800

    ADDED first code

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/*
+ *binary_tree_node -  A function that creates binary tree node
+ *@parent: is a pointer to the parent node
+ *@value: is the value to put that node
+ *Return: A pointer to the newly created node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
