08905b927d64ee0a0fc5b46b76d519489d02958b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 10:01:35 -0800

    tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - Something usefull
+ * @tree: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL)
+	{
+		leaves++;
+	}
+	if (tree->right != NULL)
+	{
+		leaves++;
+	}
+	return (leaves);
+}
