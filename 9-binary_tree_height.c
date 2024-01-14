e57829a0015293bba74be833a06fb3615c5d427b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 09:31:57 -0800

    tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
