65ba01781e1a36b5bebc34532020d27b7f8b46a2
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-01-14 08:17:18 -0800

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Something usefull
+ * @tree: Something more usefull
+ *
+ * Return: Something much more usefull
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	if (tree->left != NULL)
+	{
+		binary_tree_delete(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
+}
