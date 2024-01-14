7283045d6c6239ce4ec7c4b5ca26580849374ba9
fammad <fred.mmov@gmail.com>
2024-01-14 10:53:32 -0800

    ADDED 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
+		return (0);
 	else if ((tree->left != NULL && tree->right != NULL) ||
 		(tree->left == NULL && tree->right == NULL))
 		return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
